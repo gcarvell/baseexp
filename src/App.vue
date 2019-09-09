2e4259b204068589e570f2794f899ee08fa0d1a6
George Carvell <georgina.carvell@gmail.com>
2019-09-10 09:28:58 +1200

    Tried to do the thing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div id="header">
+			<img alt="Vue logo" src="./assets/logo.png">
+		</div>
+		<div id="demographics">
+			<p>Put your demographics here pls: <input v-model="message" placeholder="Age"></p>
+			<l>
+				<li> Generate random id </li>
+				<li> Assign condition </li>
+				<li> Fields to collect demographics </li>
+				<li> Next button </li>
+				<li> Save demographics </li>			
+			</l>
+			<hr>
+		</div>
+		<div id="instructions">
+			<l>
+				<li> Get the instruction text for the start screen </li>
+				<li> Start button </li>
+			</l>
+			<hr>
+		</div>
+		<div id="trial">
+			<l>
+				<li> Display stimuli </li>
+				<li> Collect response </li>
+				<li> Give feedback </li>
+				<li> Loop through trials </li>
+			</l>
+			<hr>
+		</div>
+		<div id="break">
+			<l>
+				<li> Get break screen text </li>
+				<li> Continue button </li>
+				<li> Loop through blocks </li>
+			</l>
+			<hr>
+		</div>
+		<div id="end">
+			<l>
+				<li> Get end screen text </li>
+				<li> Exit experiment </li>
+			</l>
+			<hr>
+		</div>
+	</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	data: {
+		butts: "Butts"
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
